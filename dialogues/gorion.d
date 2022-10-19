d7bf8ad999730959f015077eea496ba0065ca7cf
Matthew Carter <m@ahungry.com>
2022-10-18 22:01:31 -0400

    Try out a translatoin vs hardcoded

--- dialogues/gorion.d
+++ dialogues/gorion.d
@@ -1,1 +1,5 @@
-EXTEND_BOTTOM gorion 0 IF ~Global("DontRepeat","LOCALS",0)~ THEN REPLY ~(remain silent)~ DO ~SetGlobal("DontRepeat","LOCALS",1)~ GOTO 8 END
+EXTEND_BOTTOM gorion 0 IF ~Global("DontRepeat","LOCALS",0)~ THEN
+  REPLY @1
+  DO ~SetGlobal("DontRepeat","LOCALS",1)~
+  GOTO 8
+END
